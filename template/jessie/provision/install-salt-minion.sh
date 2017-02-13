4b862e3d8888b6bb6b43b2bd0c15ecbc4d1183a3
Alexander Reitzel <funtimecoding@gmail.com>
2017-02-13 13:18:26 +0100

    update jessie template to install newer salt minion

--- template/jessie/provision/install-salt-minion.sh
+++ template/jessie/provision/install-salt-minion.sh
@@ -1,46 +1,25 @@
 #!/bin/sh -e
 
-usage()
-{
-    echo "Usage: ${0} MASTER_NAME"
-}
-
 MASTER_NAME="${1}"
 
 if [ "${MASTER_NAME}" = "" ]; then
-    usage
+    echo "Usage: ${0} MASTER_NAME"
 
     exit 1
 fi
 
-USER_ID=$(id -u)
+IDENTIFIER=$(id -u)
 
-if [ ! "${USER_ID}" = "0" ]; then
-    echo "Must be run as root."
-
-    exit 1
-fi
-
-VERSION=$(cut -c 1-1 < /etc/debian_version)
-FILE=/etc/apt/sources.list.d/saltstack.list
-
-if [ "${VERSION}" = "6" ]; then
-    echo "deb http://debian.saltstack.com/debian squeeze-saltstack main" > "${FILE}"
-    BACKPORTS_SOURCE=/etc/apt/sources.list.d/backports.list
-    echo "deb http://backports.debian.org/debian-backports squeeze-backports main contrib non-free" > "${BACKPORTS_SOURCE}"
-elif [ "${VERSION}" = "7" ]; then
-    echo "deb http://debian.saltstack.com/debian wheezy-saltstack main" > "${FILE}"
-elif [ "${VERSION}" = "8" ]; then
-    echo "deb http://debian.saltstack.com/debian jessie-saltstack main" > "${FILE}"
-else
-    echo "Unsupported Debian version: ${VERSION}"
+if [ ! "${IDENTIFIER}" = 0 ]; then
+    echo "Must be root."
 
     exit 1
 fi
 
-wget -q -O- "http://debian.saltstack.com/debian-salt-team-joehealy.gpg.key" | apt-key add -
-export DEBIAN_FRONTEND="noninteractive"
-apt-get update
-apt-get -qq install salt-minion
+echo "deb http://repo.saltstack.com/apt/debian/8/amd64/latest jessie main" > /etc/apt/sources.list.d/saltstack.list
+wget --quiet --output-document - https://repo.saltstack.com/apt/debian/8/amd64/latest/SALTSTACK-GPG-KEY.pub | apt-key add -
+export DEBIAN_FRONTEND=noninteractive
+apt-get --quiet 2 update
+apt-get --quiet 2 install salt-minion
 echo "master: ${MASTER_NAME}" > /etc/salt/minion.d/minion.conf
 service salt-minion restart
